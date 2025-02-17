03ec6050b2219e59da189a4219b06720124c6ad9
jcongolo <jcongolo@student.42madrid.com>
2025-02-17 21:09:22 +0100

    actalizar split

--- ft_split.c
+++ ft_split.c
@@ -21,66 +21,88 @@
 ** ft_fill_substrings: Preenche o array de substrings com as partes divididas da string de entrada.
 ** ft_freememoria: Libera a memória alocada para as substrings em caso de erro.
 */
+//Contagem de substrings
+static int	ft_count_substrings(char const *s, char c)
+{
+	int	i;
+	int	is_substring;
+	int	num_substrings;
 
+	if (!s)
+		return (0);
+	i = 0;
+	is_substring = 0;
+	num_substrings = 0;
+	while (s[i])
+	{
+		if (s[i] != c && is_substring == 0)
+		{
+			is_substring = 1;
+			num_substrings++;
+		}
+		else if (s[i] == c)
+			is_substring = 0;
+		i++;
+	}
+	return (num_substrings);
+}
 
 // Preenche o array de substrings com as partes divididas da string de entrada 's' usando o delimitador 'c'
-int ft_fill_substrings(const char *s, char c, char **split_strings) {
-    int i;             // Índice para percorrer a string de entrada
-    int substr_start;  // Início da substring
-    int index_substr;  // Índice da substring no array
+int	ft_fill_substrings(const char *s, char c, char **split_strings)
+{
+	int	i;
+	int	substr_start;
+	int	index_substr;
 
 	i = 0;
 	substr_start = -1;
 	index_substr = 0;
-
-	// Percorre a string de entrada para criar e armazenar substrings
 	while (s[i])
 	{
 		if (s[i] != c && substr_start < 0)
 			substr_start = i;
-
 		if ((s[i] == c || s[i + 1] == '\0') && (substr_start >= 0))
 		{
-			split_strings[index_substr++] = ft_substr(s, substr_start, i - substr_start + (s[i] != c));
+			split_strings[index_substr++] = ft_substr(s, substr_start,
+					i - substr_start + (s[i] != c));
 			substr_start = -1;
-			// Verifica se a alocação de memória para a substring foi bem-sucedida
 			if (!split_strings[index_substr - 1])
 				return (0);
 		}
 		i++;
 	}
-	// Termina o array de substrings com NULL
 	split_strings[index_substr] = NULL;
 	return (1);
 }
 
 // Libera a memória alocada para as substrings
-void ft_freememoria(char **split_strings) {
-    int i = 0;
+void	ft_freememoria(char **split_strings)
+{
+	int	i;
 
-    // Libera cada substring
-    while (split_strings[i]) {
-        free(split_strings[i]);
-        i++;
-    }
-
-    // Libera o array de strings
-    free(split_strings);
+	if (!split_strings)
+ 		return;
+	i = 0;
+	while (split_strings[i])
+	{
+		free(split_strings[i]);
+		split_strings[i] = NULL;
+		i++;
+	}
+	free(split_strings);
+	split_strings = NULL;
 }
 
 // Divide a string 's' usando o delimitador 'c' e retorna um array de substrings
-char **ft_split(char const *s, char c) {
-    char **split_strings; // Array de substrings
+char	**ft_split(char const *s, char c)
+{
+	char	**split_strings;
 
-	// Verifica se 's' é NULL
 	if (!s)
 		return (0);
-
-	// Aloca memória para o array de substrings
 	split_strings = malloc((ft_count_substrings(s, c) + 1) * sizeof(char *));
 	if (!split_strings)
 		return (NULL);
-	// Preenche o array de substrings
 	if (!ft_fill_substrings(s, c, split_strings))
 	{
 		ft_freememoria(split_strings);
@@ -88,7 +110,6 @@ char **ft_split(char const *s, char c) {
 	}
 	return (split_strings);
 }
-
 /*
 int main() {
     char **result;       // Array de substrings resultantes
